21abfaa114a89d259ef41233c602a6413b0a2cc0
yuri <soaresyuri@gmail.com>
2015-10-26 11:53:29 -0300

    add filtro gaussiano

--- filtros.m
+++ filtros.m
@@ -2,7 +2,7 @@ clear % limpando variÃ¡veis
 clc % limpando console
 
 img = imread('myimage.jpg');
-img = double(rgb2gray(img));
+img = rgb2gray(img); % img = imread('myimage.jpg');
 
 [N,M] = size(img); % N=linhas da imagem; M=colunas da imagem
 F = zeros(N,M);
@@ -22,24 +22,28 @@ N2 = N+(amp_lin*2);
 M2 = M+(amp_cols*2);
 
 K = zeros(I,J);
-% TODO: inicializar o kernel com alguma coisa útil
-% K(:) = 1/(I*J) % FILTRO DA MÉDIA
+
+% K(:) = 1/(I*J) % FILTRO DA MEDIA
 
 % FILTRO GAUSSIANO
+[X,Y] = meshgrid(-amp_lin:amp_lin,-amp_cols:amp_cols);
+sigma = N/6;
+K = exp(X.^2+Y.^2)/(2*sigma^2); % normaliza ou nao?
+
 
 
 % posicionando o kernel na imagem
-% n e m iniciam no centro do kernel, referência da img
+% n e m iniciam no centro do kernel,
+% desconsideram ampliacao de borda, referencia da img
 % i e j percorrem o kernel
 for n = 1+amp_lin :1: N2-amp_lin
     for m = 1+amp_cols :1: M2-amp_cols
         result_sum_kernel = 0;
         for i = 1 :1: I
             for j = 1 :1: J
-                % dúvida: operador '*' correto?
                 index_x = n+i+amp_lin-I;
                 index_y = m+j+amp_cols-J;
-                result_sum_kernel = result_sum_kernel+ img_nova(index_x, index_y)*K(i,j);
+                result_sum_kernel = result_sum_kernel + img_nova(index_x, index_y)*K(i,j);
             end
         end
         F(n-amp_lin, m-amp_cols) = result_sum_kernel;
