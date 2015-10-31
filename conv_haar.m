61d2e964892bf055c82694d800b9daf378a4c7e4
Yuri Oliveira <soaresyuri@gmail.com>
2015-10-31 12:41:02 -0300

    adicionando piramide e laço da subamostra. o que falta?

--- conv_haar.m
+++ conv_haar.m
@@ -1,9 +1,9 @@
-tamanho_sinal = input('digite o valor do tamanho do sinal:');
-altura_piramide = input('quantos niveis quer ir na piramide?');
+tamanho_sinal = input('digite o valor do tamanho do sinal: ');
+altura_piramide = input('quantos niveis quer ir na piramide? ');
 
 N = min((log(tamanho_sinal)/log(2)),altura_piramide);
 
-VETOR = randn(1,N);
+VETOR = randn(1,tamanho_sinal);
 VETOR_AUX = VETOR;
 
 H0 = [1/sqrt(2) 1/sqrt(2)];
@@ -12,15 +12,22 @@ H1 = [1/sqrt(2) -1/sqrt(2)];
 G0 = [1/sqrt(2) 1/sqrt(2)];
 G1 = [-1/sqrt(2) 1/sqrt(2)];
 
-tamanho = tamanho_sinal/2;
-piramide = zeros(N,tamanho);
+tamanho = tamanho_sinal;
+piramide = zeros(N,tamanho/2);
 
 for i = 1 :1: N
     
-    passa_baixa = 
-    passa_alta
+    conv_h0 = conv(VETOR_AUX,H0,'same');
+    conv_h1 = conv(VETOR_AUX,H1,'same');
     
+    % laco pega a subamostra
+    k = 1;
     for j = 1 :2: tamanho
-        
+        passa_baixa(1,k) = conv_h0(1,j);
+        passa_alta(1,k) = conv_h1(1,j);
+        k = k+1;
     end
+    tamanho = tamanho/2;
+    % e agora, joel?
+    %
 end
